61e2e47310919ef477987e2459dccab754d9ac34
Исламов Азат Ильхамович <Islamov-AI@nft.local>
2020-09-25 16:52:23 +0500

    FirstVersAndFinal

---
+++ Polinom/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Polinom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("введите число");
+            string mass1 = Console.ReadLine();
+            string[] a = mass1.Split(",");
+        
+            for (int i = 0; i < a.Length; i++)
+            {
+                string FirstLine = a[i];
+                if (FirstLine != a[a.Length-1-i])
+                {
+                    Console.WriteLine("NO");
+                }
+                else
+                {
+                    Console.WriteLine("YES");
+                }
+           
+
+            }
+
+        }
+    }
+}
